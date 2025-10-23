d900bcf6b986c28cd83a327c0be614414106eee1
FoxyRaspberry <hogwartsgsrh97@gmail.com>
2025-10-23 17:17:16 +0300

    build: set up `base`

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Let-s-play-tic-tac-toe/',
+  build: { outDir: 'docs' },
   plugins: [react()],
-  build: {outDir: 'docs'},
 })
